d93e93820b494503652fa9182df01b8afa0a569d
Fabian Rodriguez <fabiancrd1995@gmail.com>
2022-10-11 21:33:52 -0500

    agregando webpack dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
+    //watch: true,
     resolve: {
         extensions: ['.js'],
         alias: {
@@ -76,5 +76,11 @@ module.exports = {
             ]
         }),
         new Dotenv(),
-    ]
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+    },
 }
